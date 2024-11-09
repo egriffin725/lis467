3cfcc043dd687c204ef938e4427d1494095a4896
egriffin725 <griffie2@simmons.edu>
2024-11-09 14:43:25 -0500

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<charset utf-8></charset>
+	<title>Profile Page</title>
+	<link href="index.css" rel="stylesheet">
+</head>
+<div id="ProfileJumbotron" class="jumbotron jumbotron-fluid">
+	<div class="row">
+		<div class="col-12">
+			<img src="https://lh3.googleusercontent.com/-ShX0GMbqgO0/VH8qDnLhurI/AAAAAAAAAas/sJvfY7eWOp0ZI9YFffcOhwRtPgDGc_JhgCL0BGAYYCw/s1000-fcrop64=1,01ab0000ffa9ffff/Youtube%2Bnew%2Bheader.png" />
+			<div class="carousel-caption">
+			<h3>Sonar Systems</h3>
+			<p>1,000,000,000 Follows :D</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-3 col-6 cardContainer">
+		<div class="card">
+			<a href="post.php"><img class="card-img-topcardImage" src="https://lh3.googleusercontent.com/-t7s6QT-8e30/AAAAAAAAAAI/AAAAAAAAAAA/sX5duDFyQ5s/w320-h180-p-rw/photo.jpg" alt="Card Image cap"></a>
+
+			<div class="card-block">
+				<a href="post.php"><h4 class="card-title">Interest 1</h4></a>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col-lg-6 col-12 postContainer">
+		<div class="card">
+			<div class="media">
+				<img class="d-flex mr-3 postThumbnail" src="https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2Fwhat-do-think-ness-surname-would-be-v0-009woyk2s58a1.png%3Fauto%3Dwebp%26s%3D44b8a964cbed5c93d0c1b28da66fdfeeb3907ed0&rdt=47144">
+
+				<div class="media-body">
+					<h5 class="mt-0">Ness from Earthbound</h5>
+					<i>1st November 2012</i>
+					<div class="postMainBody">
+					This is a description of the character Ness, from Earthbound.</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</html>
